571c83980ff4f5544f7375f955a4b3e4273104a8
Mark Soro <63247801+m-soro@users.noreply.github.com>
2022-03-23 22:01:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       console.log(entry)
-      entry.target.classList.add('animate__animated','animate__fadeInDown')
+      entry.target.classList.add('animate__animated','animate__fadeIn')
     }
   })
 }
