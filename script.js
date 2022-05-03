5e5bbf49efa352650996f333c41c9884bd8e8ceb
Mark Soro <63247801+m-soro@users.noreply.github.com>
2022-05-03 06:42:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const element = document.querySelector('.demo')
+const imglist = document.querySelectorAll(".anim");    
 
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      console.log('Element is entering the screen',entry)
-      element.style.visibility = "visible"
-      entry.target.classList.add('animate__animated','animate__fadeIn', 'animate__slow')
+      console.log(entry)
+      for (const pic of imglist) { 
+         pic.style.visibility = "visible"
+        }
+      entry.target.classList.add('animate__animated','animate__fadeIn')
          
     }
   })
@@ -13,4 +15,25 @@ const callback = (entries, observer) => {
 
 const myObserver = new IntersectionObserver(callback, {threshold: 0.0000001})
 
-myObserver.observe(element)
+for (const pic of imglist) { 
+  myObserver.observe(pic)
+ }
+
+
+
+// const element = document.querySelector('.demo')
+
+// const callback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       console.log('Element is entering the screen',entry)
+//       element.style.visibility = "visible"
+//       entry.target.classList.add('animate__animated','animate__fadeIn', 'animate__slow')
+         
+//     }
+//   })
+// }
+
+// const myObserver = new IntersectionObserver(callback, {threshold: 0.0000001})
+
+// myObserver.observe(element)
